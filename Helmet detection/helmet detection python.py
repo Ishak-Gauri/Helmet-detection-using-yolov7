9f1b7151473c408cfaeab0bd1c7b65ea6b339360
Ishak-Gauri <142309681+Ishak-Gauri@users.noreply.github.com>
2024-01-18 23:17:44 +0545

    Add files via upload

---
+++ Helmet detection/helmet detection python.py
@@ -0,0 +1,14 @@
+from roboflow import Roboflow
+
+rf = Roboflow(api_key="cfA6ghDzYXzeJKKNqQGp")
+project = rf.workspace().project("helmet-detecetion")
+model = project.version(1).model
+
+# infer on a local image
+print(model.predict("000083_jpg.rf.cb7a594501b4c44e2069aa15212024c4.jpg", confidence=40, overlap=30).json())
+
+# visualize your prediction
+model.predict("000083_jpg.rf.cb7a594501b4c44e2069aa15212024c4.jpg", confidence=40, overlap=30).save("prediction.jpg")
+
+# infer on an image hosted elsewhere
+# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
